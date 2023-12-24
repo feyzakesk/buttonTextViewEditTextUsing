3ff46f72b950366e40a659596872d2757d33b0b7
feyzakesk <feyzaaa3455@gmail.com>
2023-12-24 13:41:40 +0300

    ilk commit

---
+++ app/src/main/java/com/feyzakesk/buttontextviewedittextusing/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.feyzakesk.buttontextviewedittextusing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feyzakesk.buttontextviewedittextusing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val alinanVeri = binding.editText.text.toString()
+            binding.textView.text = alinanVeri
+        }
+
+
+    }
+}
